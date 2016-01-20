546ee7f183ce4d03edacafc47cfaed6e7e459ddc
UENISHI Kota <kota@basho.com>
2016-01-20 14:44:11 +0900

    Update clojure version and dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/kuenishi/kudu-clj"
   :license {:name "Apache License 2"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.kududb/kudu-client "0.5.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.kududb/kudu-client "0.6.0"]]
                   ;:exclusions [org.slf4j/slf4j-api]]]
   :repositories [["Cloudera repo"
                   "https://repository.cloudera.com/artifactory/repo/"]]
